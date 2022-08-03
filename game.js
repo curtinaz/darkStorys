bef24973b89647054ba6e1b60bb0af6a32078cfe
Cristian Curtinaz <curtinazif@gmail.com>
2022-08-03 17:47:54 -0300

    Generate working

--- game.js
+++ game.js
@@ -1,7 +1,14 @@
 var generateButton = document.querySelector("#generateCard");
 
 generateButton.addEventListener('click', () => {
-    console.log('clicou');
 
-    
+    axios('http://localhost/darkStories-api/public/api/cards/random')
+        .then(function (response) {
+            var dados = response.data;
+
+            document.querySelector("#title").innerText = dados.title
+            document.querySelector("#description").innerText = dados.description
+
+        });
+
 })
