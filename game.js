bb16570677a62ceb67257973639df848c53a9355
Cristian Curtinaz <curtinazif@gmail.com>
2022-08-04 17:11:43 -0300

    flipCard

--- game.js
+++ game.js
@@ -1,13 +1,19 @@
 var generateButton = document.querySelector("#generateCard");
+var initiate = false;
 
 generateButton.addEventListener('click', () => {
 
     axios('https://curtinaz.dev/darkStories-api/public/api/cards/random')
         .then(function (response) {
             var dados = response.data;
+            initiate = true;
+
+            $("#answer").hide(200);
+            $("#hint").show(200);
 
             document.querySelector("#title").innerText = dados.title
             document.querySelector("#description").innerText = dados.description
+            document.querySelector("#answer").innerText = dados.answer
 
         });
 
